bc7f67f42af9ebe3da30edb02278f84214d8981f
KBCHANUKAJ <chanukakbc@gmail.com>
2023-04-07 20:23:54 +0530

    Update WebSecurityConfig.java

--- Spring Boot Login and Registration/src/main/java/com/login_reistration/security/WebSecurityConfig.java
+++ Spring Boot Login and Registration/src/main/java/com/login_reistration/security/WebSecurityConfig.java
@@ -65,7 +65,6 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authorizeRequests().antMatchers("/api/auth/**").permitAll()
         .antMatchers("/api/test/**").permitAll()
-            .antMatchers("/user/**").hasRole("admin")
         .anyRequest().authenticated();
     
     http.authenticationProvider(authenticationProvider());
